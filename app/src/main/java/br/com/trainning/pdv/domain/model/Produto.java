d55de06903de69b6afacef435549086ec663e596
android <julianosakai@gmail.com>
2015-11-28 16:28:25 -0200

    Commit1

--- app/src/main/java/br/com/trainning/pdv/domain/model/Produto.java
+++ app/src/main/java/br/com/trainning/pdv/domain/model/Produto.java
@@ -4,4 +4,13 @@ package br.com.trainning.pdv.domain.model;
  * Created by android on 28/11/2015.
  */
 public class Produto {
+
+    private Long id;
+    private String descricao;
+    private String unidade;
+    private String codigoBarras;
+    private Double preco;
+    private int ativo;
+
+
 }
